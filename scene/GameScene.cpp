b92e1e48d66e6bc33f19e11348588530fe64e3a3
LC1A_02_asuma <k021g1466@g.neec.ac.jp>
2023-01-20 14:16:17 +0900

    a

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -17,40 +17,6 @@ void GameScene::Initialize(DirectXCommon* dxCommon, Input* input)
 	Camera* newCamera = new Camera();
 	newCamera->Initialize(input_,dxInput, XMFLOAT3(0,0,-10));
 	camera_.reset(newCamera);
-
-	// FbxManager作成
-	FbxManager* fbx_manager = FbxManager::Create();
-
-	// FbxImporter作成
-	FbxImporter* fbx_importer = FbxImporter::Create(fbx_manager, "ImportTest");
-	if (fbx_importer == nullptr)
-	{
-		fbx_manager->Destroy();
-	}
-
-	// FbxScene作成
-	FbxScene* fbx_scene = FbxScene::Create(fbx_manager, "SceneTest");
-	if (fbx_scene == nullptr)
-	{
-		fbx_importer->Destroy();
-		fbx_manager->Destroy();
-	}
-
-	// ファイルを初期化する
-	if (fbx_importer->Initialize("Box.fbx") == false)
-	{
-		fbx_importer->Destroy();
-		fbx_scene->Destroy();
-		fbx_manager->Destroy();
-	}
-
-	// インポート
-	/*if (fbx_importer->Import(fbx_scene) == false)
-	{
-		fbx_importer->Destroy();
-		fbx_scene->Destroy();
-		fbx_manager->Destroy();
-	}*/
 }
 
 void GameScene::Update()
